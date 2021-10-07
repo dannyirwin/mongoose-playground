586d9a80dcbf8cf3b8e39856388e0845faac7687
dannyirwin <dirwin123@gmail.com>
2021-10-07 13:43:21 -0600

    finished"

--- index.js
+++ index.js
@@ -6,9 +6,26 @@ const app = express();
 const PORT = 4000;
 const clusterConnection =
   'mongodb+srv://Dirwin:1234@cluster0.gvcxl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const { Schema } = mongoose;
+
+const pirateSchema = new Schema({
+  name: String,
+  age: Number
+});
+
+const Pirate = mongoose.model('Pirate', pirateSchema);
 
 app.use(cors());
 app.use(express.json());
+
+app.post('/pirates', (request, response) => {
+  const { pirate } = request.body;
+  Pirate.create(pirate).then(pirate => response.send(pirate));
+});
+
+app.get('/pirates', (_, response) => {
+  Pirate.find({}).then(pirates => response.send(pirates));
+});
 
 mongoose
   .connect(clusterConnection, {
